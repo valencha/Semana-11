3b76e857e6d268ccf8e7744e9c952f888a551dbc
valentinachavez <valencha10@gmail.com>
2019-04-05 07:34:04 -0500

    prueba

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 var express = require('express');
 var app = express();
 
+console.log(__dirname);
+
+var fs = require ('fs');
+
+app.use(express.static('public'));
+var contador =0;
+
 app.get('/', function(req, res) {
-  res.send('Hola Mundo!');
+  res.sendFile(__dirname+'/public/home.html');
+  contador ++;
+  console.log('pagina visitada'+ contador);
+
+
+fs.writeFile('contador.txt','pagina visita: '+contador , 'utf8',function(){
+console.log('archivo escrito');
 });
 
-app.get('/tienda', function(request, response) {
-    response.send('Funciona!');
+});
+
+app.get('/contacto', function(request, response) {
+    response.sendFile(__dirname+'/public/contacto.html');
+  });
+
+  app.get('/sobre-mi', function(request, response) {
+    response.sendFile(__dirname+'/public/sobremi.html');
+  });
+
+
+  app.get('/portafolio', function(request, response) {
+    response.sendFile(__dirname+'/public/portafolio.html');
   });
   
 
 app.listen(3000, function() {
   console.log('Aplicación ejemplo, escuchando el puerto 3000!');
 });
+
+////FS
